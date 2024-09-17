f222052ca994adc06be445050ea1bd01f196564e
steeski <167318267+steeski@users.noreply.github.com>
2024-09-17 11:02:53 -0300

    aaaaaaaaaa

---
+++ perguntas.js
@@ -0,0 +1,126 @@
+criaCartao {                    
+    'matematica';
+    'Roberta resolve passear com a sua filhinha Malu e, antes de sair de seu apartamento, decide vesti-la com uma camiseta e uma calça. Se Malu tem 7 camisetas e 3 calças, todas distintas, de quantas maneiras Roberta pode escolher uma camiseta e uma calça para vestir a Malu?';
+    '21';}
+    
+    criaCartao {
+    'matematica';
+    'Numa lanchonete o lanche é composto por três partes: pão, molho e recheio. Se essa lanchonete oferece aos seus clientes duas opções de pão, três de molho e quatro de recheio, a quantidade de lanches distintos que ela pode oferecer é de';
+    '24';}
+    
+    criaCartao {
+    'matematica';
+    'Em fevereiro, o governo da Cidade do México, metrópole com uma das maiores frotas de automóveis do mundo, passou a oferecer à população bicicletas como opção de transporte. Por uma anuidade de 24 dólares, os usuários têm direito a 30 minutos de uso livre por dia. O ciclista pode retirar em uma estação e devolver em qualquer outra e, se quiser estender a pedalada, paga 3 dólares por hora extra.
+    Revista Exame. 21 abr. 2010.
+    A expressão que relaciona o valor f pago pela utilização da bicicleta por um ano, quando se utilizam x horas extras nesse período é';
+    'f(x) =3x + 24';}
+    
+    criaCartao {
+    'física';
+    'Para a proteção contra curtos-circuitos em residências são utilizados disjuntores, compostos por duas lâminas de metais diferentes, com suas superfícies soldadas uma à outra, ou seja, uma lâmina bimetálica. Essa lâmina toca o contato elétrico, fechando o circuito e deixando a corrente elétrica passar. Quando da passagem de uma corrente superior à estipulada (limite), a lâmina se curva para um dos lados, afastando-se do contato elétrico e, assim, interrompendo o circuito. Isso ocorre porque os metais da lâmina possuem uma característica física cuja resposta é diferente para a mesma corrente elétrica que passa no circuito.
+     A característica física que deve ser observada para a escolha dos dois metais dessa lâmina bimetálica é o coeficiente de';
+    'dilatação térmica';}
+    
+    criaCartao {
+    'física';
+    '“Isso é apenas a ponta do iceberg” é uma metáfora utilizada em contextos onde há mais informação sobre um determinado fato do que se pode perceber de imediato. Essa analogia é possível pois 90% de cada um desses blocos de gelo estão submersos, ou seja, não estão visíveis.
+    Essa característica está associada à seguinte propriedade física do iceberg';
+    'densidade';}
+    
+    criaCartao {
+    'física';
+    'As grandezas físicas escalares são expressas apenas pelo seu valor numérico e unidade de medida. As grandezas físicas vetoriais além do valor numérico e unidade de medida, para serem expressas, necessitam de direção e sentido.
+    Com base nisso, assinale a opção que corresponde a uma grandeza física de natureza vetorial.';
+    'força';}
+    
+    criaCartao {
+    'química';
+    'Bebidas podem ser refrigeradas de modo mais rápido utilizando-se caixas de isopor contendo gelo e um pouco de sal grosso comercial. Nesse processo ocorre o derretimento do gelo com consequente formação de líquido e resfriamento das bebidas. Uma interpretação equivocada, baseada no senso comum, relaciona esse efeito à grande capacidade do sal grosso de remover calor do gelo.
+     Do ponto de vista científico, o resfriamento rápido ocorre em razão da';
+    'diminuição da temperatura de fusão do líquido.';}
+    
+    criaCartao {
+    'química';
+    'O cientista Richard Feynman, prêmio Nobel de Física em 1965, fez comentários sobre o processo de combustão em uma entrevista chamada Fun to Imagine. Segundo ele, à primeira vista, é impressionante pensar que os átomos de carbono de uma árvore não entram em combustão com o oxigênio da atmosfera de forma espontânea, já que existe uma grande afinidade entre essas espécies para a formação de CO2. Entretanto, quando a reação tem início, o fogo se espalha facilmente.
+     Essa aparente contradição pode ser explicada pois';
+    'a reação precisa de uma energia de ativação para começar.';}
+    
+    criaCartao {
+    'química';
+    'Vazamento de petróleo, um drama recorrente
+    Ao cair no ambiente marinho, por exemplo, o petróleo impede a passagem de luz. Isso afeta de maneira imediata o fitoplâncton, organismos fotossintetizantes e que, portanto, necessitam de luminosidade. Com a redução do fitoplâncton, o zooplâncton, que se alimenta desses organismos, acaba tendo sua reserva de alimento reduzida. Desse modo, o petróleo afeta de maneira negativa toda a cadeia alimentar.
+     Os impactos decorrentes desse derramamento ocorrem porque os componentes do petróleo';
+    'têm caráter hidrofóbico e baixa densidade, impedindo as trocas gasosas entre o meio aquático e a atmosfera.';}
+    
+    
+    criaCartao {
+    'biologia';
+    'Durante as estações chuvosas, aumentam no Brasil as campanhas de prevenção à dengue, que têm como objetivo a redução da proliferação do mosquito Aedes aegypti, transmissor do vírus da dengue. Que proposta preventiva poderia ser efetivada para diminuir a reprodução desse mosquito?';
+    'Remoção dos recipientes que possam acumular água, porque as larvas do mosquito se desenvolvem nesse meio.';}
+    
+    criaCartao {
+    'biologia';
+    'Nos últimos 60 anos, a população mundial duplicou, enquanto o consumo de água foi multiplicado por sete.  Da água existente no planeta, 97% são água salgada (mares e oceanos), 2% formam geleiras inacessíveis e apenas 1% corresponde à água doce, armazenada em lençóis subterrâneos rios e lagos. A poluição pela descarga de resíduos municipais e industriais, combinada com exploração excessiva dos recursos hídricos disponíveis, ameaça o meio ambiente, comprometendo a disponibilidade de água doce para o abastecimento das populações humanas. Se esse ritmo se mantiver em alguns anos à água potável tornar-se-á um bem extremamente raro e caro. Considerando o texto, uma proposta viável para conservar o meio ambiente e água doce seria';
+    'Reduzir a poluição e exploração dos recursos naturais, otimizar o uso da água potável e aumentar a captação da água da chuva.';}
+    
+    criaCartao {
+    'biologia';
+    'Recentemente, a mídia vem noticiando o enorme aumento do buraco de ozônio sobre o continente Antártico e áreas adjacentes. As populações humanas do sul do Chile e da Argentina estão sendo aconselhadas a não se manterem no sol durante as horas mais quentes do dia para que não estejam sujeitas a sofrer';
+    'queimaduras e câncer de pele provocados pelo excesso de radiação ultravioleta.';}
+    
+    criaCartao {
+    'história';
+    'Entende-se comumente por Monarquia aquele sistema de dirigir que se centraliza estavelmente numa só pessoa investida de poderes especialíssimos, exatamente monárquicos, que a colocam claramente acima de todo o conjunto dos governados. 
+    (BOBBIO, Noberto. Dicionário de política. Brasília UNB, 1995) 
+     A forma de organização política exposta acima refere-se:';
+    'Ao absolutismo monárquico.';}
+    
+    criaCartao {
+    'história';
+    'O iluminismo (ou ilustração) foi uma corrente de ideias que teve origem no século XVII e se desenvolveu sobretudo no século XVIII. O referido movimento é considerado importante por transformar a visão tradicional do homem moderno. 
+     O iluminismo expressou a';
+    'consolidação do uso da razão, ou racionalismo, como elemento essencial do ser humano.';}
+    
+    criaCartao {
+    'história';
+    'No ano de 1945, bombas nucleares destruíram as cidades japonesas de Hiroshima e Nagasaki, impressionando o mundo pelo seu poder de devastação.
+    Assinale a alternativa que corresponde, de forma correta e respectivamente, a guerra em questão e ao país que lançou as bombas.';
+    'Segunda Guerra Mundial e Estados Unidos.';}
+    
+    criaCartao {
+    'geografia';
+    'Eu estava pagando o sapateiro e conversando com um preto que estava lendo um jornal. Ele estava revoltado com um guarda civil que espancou um preto e amarrou numa árvore. O guarda civil é branco. E há certos brancos que transforma o preto em bode expiatório. Quem sabe se guarda civil ignora que já foi extinta a escravidão e ainda estamos em regime de chibata?
+    JESUS, C. M. Quarto de despejo: diário de uma favelada. São Paulo: Ática, 2014.
+     O texto que guarda a grafia original da autora, expõe uma característica da sociedade brasileira, que é o(a):';
+    'racismo estrutural';}
+    
+    criaCartao {
+    'geografia';
+    'Segundo estudos, o aquecimento global é responsável pelas mudanças climáticas que vêm ocorrendo em todo o planeta, com trágicas consequências ambientais, econômicas e sociais.
+     No que concerne ao aquecimento global, assinale a afirmação verdadeira.';
+    'Em decorrência do aquecimento global, milhões de pessoas poderão enfrentar fome e racionamento de água, além das consequências do desaparecimento de muitas áreas costeiras pela elevação do nível do mar.';}
+    
+    criaCartao {
+    'geografia';
+    'O desastre de Chernobyl ainda custa caro. Hoje o governo gasta até 7% dos impostos para garantir o isolamento e a segurança de uma região maior que um Parque do Ibirapuera e meio. O país já aceitou que a Zona de Exclusão não vai servir para moradia, plantação nem para a atividade madeireira tão cedo.
+    www.superinteressante.com.br. Adaptado.
+     O acidente e o país relacionados ao fato mencionado no excerto correspondem, respectivamente,';
+    'à explosão de uma usina nuclear e à Ucrânia.';}
+    
+    criaCartao {
+    'sociologia';
+    'Eu estava pagando o sapateiro e conversando com um preto que estava lendo um jornal. Ele estava revoltado com um guarda civil que espancou um preto e amarrou numa árvore. O guarda civil é branco. E há certos brancos que transforma o preto em bode expiatório. Quem sabe se guarda civil ignora que já foi extinta a escravidão e ainda estamos em regime de chibata?
+    JESUS, C. M. Quarto de despejo: diário de uma favelada. São Paulo: Ática, 2014.
+     O texto que guarda a grafia original da autora, expõe uma característica da sociedade brasileira, que é o(a):';
+    'racismo estrutural';}
+    
+    
+    criaCartao {
+    'sociologia';
+    'A sociedade burguesa moderna, que brotou das ruínas da sociedade feudal, não aboliu os antagonismos de classes. Não fez senão substituir velhas classes, velhas condições de opressão, velhas formas de luta por outras novas. Entretanto, a nossa época, a época da burguesia, caracteriza-se por ter simplificado os antagonismos de classes.
+    MARX, K.; ENGELS, F. O manifesto comunista. São Paulo: Paz e Terra, 1998.
+     Na perspectiva dos autores, os antagonismos entre as classes sociais no capitalismo decorrem da separação entre aqueles que detêm os meios de produção e aqueles que';
+    'vendem a força de trabalho.';}
+    
+    
+    
