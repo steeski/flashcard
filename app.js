01a08a8d7bd680cf1e7fed586656a8ea4b4de4a4
steeski <167318267+steeski@users.noreply.github.com>
2024-09-24 10:47:54 -0300

    s

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-function criaCartao(categoria, pergunta, resposta){
-    let container = document.getElementById('container');
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao';
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
     cartao.innerHTML = `
-    <div class="cartao_conteudo">
+    <div class="cartao__conteudo">
     <h3>${categoria}</h3>
-    <div class="cartao_conteudo_pergunta">
-    <p>${pergunta}</p>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
     </div>
-    <div class="cartao_conteudo_resposta">
+    <div class="cartao__conteudo__resposta">
         <p>${resposta}</p>
-    </div>`
-    container.appendChild(cartao);
+    </div>
+    </div>
+    `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
+
 }
