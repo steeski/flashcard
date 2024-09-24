9a114a44e3e491dd332c6720ccc2bd78c2323190
steeski <167318267+steeski@users.noreply.github.com>
2024-09-24 09:12:50 -0300

    aaaaa

---
+++ app.js
@@ -0,0 +1,13 @@
+function criaCartao(categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class="cartao_conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao_conteudo_pergunt">
+    <p>${pergunta}</p>
+    </div>`
+    container.appendChild(cartao);
+
+}
