a12cf49984ed3380eeddc087dfc6a250e0005bc8
steeski <167318267+steeski@users.noreply.github.com>
2024-10-01 09:35:48 -0300

    aaaaaaaaaa

--- perguntas.js
+++ perguntas.js
@@ -1,100 +1,99 @@
-criaCartao (
-    'Matematica' ,
+criaCartao(
+    'Matematica',
     ' Qual é a fórmula da área de um triângulo? ',
-     'A = (basexaltura)/2')
-    
-    criaCartao (
+    'A = (basexaltura)/2')
+
+criaCartao(
     'Matematica',
     'O que é uma função linear?',
     ' É uma função que pode ser escrita na forma f(x)= mx + b')
-    
-    criaCartao (
+
+criaCartao(
     'Matemática',
     'Qual é a fórmula da soma dos ângulos internos de um triângulo?',
     '180 graus')
-    
-    criaCartao (
+
+criaCartao(
     'Fisica',
     'Qual é a fórmula da segunda lei de Newton?',
     'F = mxa (força = massaxaceleração')
-    
-    criaCartao (
+
+criaCartao(
     'Fisica',
     'O que é a energia cinética?',
     'É a energia de um corpo em movimento, dada por EK = (1/2)mv²')
-    
-    criaCartao (
+
+criaCartao(
     'Física',
     'Qual é a fórmula para calcular a pressão?',
     'P = F/A (pressão = força / área)')
-    
-    criaCartao (
+
+criaCartao(
     'Quimica',
     'O que é uma reação de oxidação?',
     'É a perda de elétrons por uma substância durante uma reação química')
-    
-    criaCartao (
+
+criaCartao(
     'Quimica',
     'O que são ácidos e bases segundo a teoria de Arrhenius?',
     'Ácidos liberam íons H⁺ em solução, e bases liberam íons OH⁻')
-    
-    criaCart (
-    'Quimica'
-    ), 
-    'Qual é a fórmula da reação de neutralização entre um ácido e uma base?'
-    ' HA + BOH -> BA + H₂O (ácido + base -> sal + água)'
-    
-    criaCartao (
+
+criaCart(
+    'Quimica',
+    'Qual é a fórmula da reação de neutralização entre um ácido e uma base?',
+' HA + BOH -> BA + H₂O (ácido + base -> sal + água)')
+
+criaCartao(
     'Biologia',
     'O que é a fotossíntese?',
     'É o processo pelo qual as plantas convertem luz solar em energia química, usando dióxido de carbono e água, gerando glicose e oxigênio.')
-    
-    criaCartao (
+
+criaCartao(
     'Biologia',
     'Qual é a unidade básica da vida?',
     'A célula')
-    
-    criaCartao (
+
+criaCartao(
     'Biologia',
     'O que são organismos autotróficos?',
     'Organismos que produzem seu próprio alimento, como plantas, utilizando a fotossíntese ou quimiossíntese')
-    
-    criaCartao (
+
+criaCartao(
     'Historia',
     'O que foi a Revolução Industrial?',
     'Um período de transição para novos processos de produção, que começou no século XVIII, caracterizado pela mecanização e pela industrialização')
-    
-    criaCartao (
+
+criaCartao(
     'Historia',
     'O que foi a Guerra Fria?',
     'Um período de tensão política e militar entre os Estados Unidos e a União Soviética, que ocorreu após a Segunda Guerra Mundial, sem um confronto direto entre as superpotências.')
-    
-    criaCartao (
+
+criaCartao(
     'Historia',
     'Qual foi a causa da Primeira Guerra Mundial?',
     'A rivalidade imperialista, alianças militares e o assassinato do arquiduque Francisco Ferdinando em 1914.')
-    
-    criaCartao (
+
+criaCartao(
     'Geografia',
     'O que é uma bacia hidrográfica?',
     'É uma área de terra onde toda a água que cai se acumula e escoa para um único corpo de água como um rio ou lago.')
-    
-    criaCartao (
+
+criaCartao(
     'Geografia',
     'Qual é a diferença entre clima e tempo?',
     'O clima é a média das condições atmosféricas em uma região ao longo do tempo, enquanto o tempo se refere às condições atmosféricas em um determinado momento.')
-    
-    criaCartao (
+
+criaCartao(
     'Geografia',
     'O que é urbanização?',
     'É o processo de crescimento das cidades e a migração da população rural para áreas urbanas, resultando em aumento da densidade populacional nas cidades.')
-    
-    criaCartao (
+
+criaCartao(
     'Sociologia',
     'O que é socialização?',
     'O processo pelo qual os indivíduos aprendem e internalizam as normas, valores e comportamentos da sua cultura.')
-    
-    criaCartao (
+
+criaCartao(
     'Sociologia',
     'o que é uma instituição social?',
     'Estruturas organizadas que desempenham funções importantes na sociedade, como família, educação, religião e governo.')
